a5fa05bc0a93b3d1d7fe55f8bca4a6a9f605f2fb
silver0510 <phunghia91bkdn@gmail.com>
2022-02-22 16:03:44 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-3gkyf3-azn4wp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-3gkyf3-azn4wp)
---
+++ src/main.jsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './App';
+import Expenses from './routes/expenses';
+import Invoices from './routes/invoices';
+import './main.css';
+import Invoice from './routes/invoice';
+
+let rootElement = document.getElementById('root');
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route
+          index
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>Main page </p>
+            </main>
+          }
+        />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route
+            index
+            element={
+              <main style={{ padding: '1rem' }}>
+                <p>Select an invoice </p>
+              </main>
+            }
+          />
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p> There's nothing here! </p>
+            </main>
+          }
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
